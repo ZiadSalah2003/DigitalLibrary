9bb1a359eb82f96e34ac7677b24987cc13929f91
Ziad Salah <ziadsalah2003@gmail.com>
2023-12-07 22:27:32 +0200

    Upade App.js File To Display LoginPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Login from "./Components/Login/Login";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <div>
+       <Login/>
+      </div>
   );
 }
 
